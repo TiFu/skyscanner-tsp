414b6fe7d6cf4cdb73989a61a175dd8fd2bb6ecf
David Duong <david@duong.cz>
2018-10-20 00:22:05 +0200

    fix

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import GoogleMap from 'google-map-react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GoogleMap
+        
+        />
+      </div>
+    )
+  }
+}
+
+export default App
