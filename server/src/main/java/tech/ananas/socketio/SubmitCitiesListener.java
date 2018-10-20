57098cb304cead0257def0c6b25ff2668ed94a15
Tino Fuhrmann <tino-fuhrmann@web.de>
2018-10-20 17:42:11 +0200

    Set name to unknown if not present in restore session

--- server/src/main/java/tech/ananas/socketio/SubmitCitiesListener.java
+++ server/src/main/java/tech/ananas/socketio/SubmitCitiesListener.java
@@ -20,6 +20,9 @@ public class SubmitCitiesListener implements DataListener<SubmitCityListRequest>
 	@Override
 	public void onData(SocketIOClient client, SubmitCityListRequest data, AckRequest ackSender) throws Exception {
 		Session session = this.server.getSessionService().getSession(data.getId());
+		if (!client.has("name")) {
+			client.set("name", "Unknown");
+		}
 		Route r = session.addRoute(new User(client.get("name")), data);
 		// TODO: recalculate costs
 		if (!this.server.getFlightsService().updateTrip(r)) {
